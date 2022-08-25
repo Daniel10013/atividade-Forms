8daa3d49b4ae98b5ec101fbe1f3dd5b225fcce91
DANIEL_FILIPE_SOARES_CORREA_VILACA <12102644@OMEGAB21.BARROCA>
2022-08-25 09:42:18 -0300

    Primeira parte do front

---
+++ front/src/components/form.tsx
@@ -0,0 +1,11 @@
+import "./form.css"
+
+function form(){
+    return(<form>
+        <input type="text" placeholder="Insira o nome:" />
+        <input type="text" placeholder="Insira o email:" />
+        <button>Salvar </button>
+    </form>)
+}
+
+export default form;
